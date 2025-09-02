f4d061e6173309285f8d6b5181092afa9b2c014e
inno <innocentiahan14@gmail.com>
2025-09-02 12:49:03 +0800

    done

--- practicum-w1.php
+++ practicum-w1.php
@@ -1,7 +1,34 @@
 <?php
-    $array1 = [1,2,3,4,5,6];
-    $array2 = [6,7];
+    $array1 = [0,1,2,3,4,0,0,3];
+    $array2 = [6,7,2,1];
 
-    $mergedArray = array_merge($array1, $array2);
-    echo implode(", ", $mergedArray);
+    $tagged = [];
+
+    foreach ($array1 as $num){
+        if ($num != 0){
+            $tagged[] = ['value' => $num, 'from' => 1];
+        }
+    }
+
+    foreach ($array2 as $num){
+        if ($num != 0){
+            $tagged[] = ['value' => $num, 'from' => 2];
+        }
+    }
+
+    usort($tagged, function($a,$b){
+        return $a['value'] <=> $b['value'];
+    });
+
+    $output = [];
+
+    foreach ($tagged as $item) {
+        if ($item['from'] == 1) {
+            $output[] =  "<u>{$item['value']}</u>";
+        } else {
+            $output[] = "{$item['value']}";
+        }
+    }
+
+    echo "[". implode(", ", $output) . "]";
 ?>
