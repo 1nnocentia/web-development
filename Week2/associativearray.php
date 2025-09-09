6f1b8d3b315a0339a526814db9c461093be65a1f
inno <innocentiahan14@gmail.com>
2025-09-09 10:19:01 +0800

    week 2

---
+++ Week2/associativearray.php
@@ -0,0 +1,13 @@
+<?php
+    $gereja = [
+       "hari"  => "minggu",
+       "misa ke-" => "1",
+       "waktu" => "06.30"
+    ];
+
+    echo $gereja["hari"];
+    echo " ";
+    echo $gereja["misa ke-"];
+    echo " : ";
+    echo $gereja["waktu"];
+?>
